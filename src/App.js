7817da360fcb19c429f91f27f0d7aeb3dc747c2e
VIkash2205 <VIkash2205@users.noreply.github.com>
2023-08-30 14:50:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import About from './Components/About';
+import Footer from './Components/Footer';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+import Tours from './Components/Tours';
+import Services from './Components/Services';
+
+
+
+function App() {
+  return (
+    <>
+   <Navbar />
+   <Hero />
+   <About />
+   <Footer />
+   <Services />
+   <Tours />
+
+
+
+</>
+   
+  );
+}
+
+export default App;
